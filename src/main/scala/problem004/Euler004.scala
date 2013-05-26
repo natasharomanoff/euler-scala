4aff4cdf3349ec189ad4185d92d81dce30637c0a
Natasha Romanoff <black.widow@techie.com>
2013-05-26 16:18:51 +0530

    problem 004 solution

--- src/main/scala/problem004/Euler004.scala
+++ src/main/scala/problem004/Euler004.scala
@@ -4,13 +4,34 @@ package problem004
 /* 
  * Largest palindrome product
  * 
- * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+ * A palindromic number reads the same both ways. 
+ * 
+ * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+ * 
  * Find the largest palindrome made from the product of two 3-digit numbers. 
  */
 
 object Euler004 {
 
-  def main(args: Array[String]): Unit = { println("Result is ") }
+  def reverse(n: Int): Int = {
+    def revh(n: Int, acc: Int): Int = if (n == 0) acc else revh(n / 10, (10 * acc) + (n % 10))
+    revh(n, 0)
+  }
+
+  def isPalindrome(n: Int): Boolean = n == reverse(n)
+
+  def largestPalindrome(digits: Int): Int = {
+    def ubound(digits: Int): Int = (math.pow(10, digits) - 1).toInt
+    def lbound(digits: Int): Int = math.pow(10, digits - 1).toInt
+
+    (for {
+      i <- Stream.range(ubound(digits), lbound(digits), -1)
+      j <- Stream.range(ubound(digits), lbound(digits), -1)
+      if (isPalindrome(i * j))
+    } yield (i * j)).max
+  }
+
+  def main(args: Array[String]): Unit = { println("Result is " + largestPalindrome(3)) }
 
 }
 	
