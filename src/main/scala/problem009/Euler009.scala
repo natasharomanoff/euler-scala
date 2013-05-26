71857b18b9d8fd29cb14f8ce37a3a15ec5971260
Natasha Romanoff <black.widow@techie.com>
2013-05-26 18:57:07 +0530

    problem 009 solution

--- src/main/scala/problem009/Euler009.scala
+++ src/main/scala/problem009/Euler009.scala
@@ -5,14 +5,29 @@ package problem009
  * Special Pythagorean triplet
  * 
  * A Pythagorean triplet is a set of three natural numbers, a b c, for which,
- * a2 + b2 = c2
- * For example, 32 + 42 = 9 + 16 = 25 = 52.
- * There exists exactly one Pythagorean triplet for which a + b + c = 1000.Find the product abc. 
+ * a^2 + b^2 = c^2
+ * 
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * 
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * 
+ * Find the product abc. 
  */
 
 object Euler009 {
 
-  def main(args: Array[String]): Unit = { println("Result is ") }
+  def sqr(a: Int): Int = a * a
+
+  def isPythagoreanTriplet(a: Int, b: Int, c: Int): Boolean = sqr(a) + sqr(b) == sqr(c)
+
+  def tripletWithSum(s: Int): List[Int] = (for {
+    a <- 1 to s
+    b <- 1 to (s - a)
+    c = (s - a - b)
+    if isPythagoreanTriplet(a, b, c)
+  } yield (List(a, b, c))).head 
+
+  def main(args: Array[String]): Unit = { println("Result is " + tripletWithSum(1000).product ) }
 
 }
 	
