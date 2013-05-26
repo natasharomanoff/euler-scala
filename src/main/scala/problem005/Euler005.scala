868f740196064c51d01056f80c31b814f6065a6d
Natasha Romanoff <black.widow@techie.com>
2013-05-26 16:31:52 +0530

    problem 005 solution

--- src/main/scala/problem005/Euler005.scala
+++ src/main/scala/problem005/Euler005.scala
@@ -4,13 +4,25 @@ package problem005
 /* 
  * Smallest multiple
  * 
- * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
- * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? 
+ * 2520 is the smallest number that can be divided by 
+ * each of the numbers from 1 to 10 without any remainder.
+ * 
+ * What is the smallest positive number that is evenly divisible 
+ * by all of the numbers from 1 to 20? 
  */
 
 object Euler005 {
 
-  def main(args: Array[String]): Unit = { println("Result is ") }
+  def gcd(a: Long, b: Long): Long = if (b == 0) a else gcd(b, a % b)
+
+  def lcm(a: Long, b: Long): Long = (a * b) / gcd(a, b)
+
+  def minEvenlyDivisible(divisors: List[Long]) = divisors.foldLeft(1L)(lcm(_, _))
+ 
+  def solution: Long = minEvenlyDivisible((1L to 20L).toList)
+
+  def main(args: Array[String]): Unit = { println("Result is " + solution ) }
+  
 
 }
 	
